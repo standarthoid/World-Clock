bde19223844ea115f868a7a5d1083718a2288b43
standarthoid <90977509+standarthoid@users.noreply.github.com>
2022-11-17 10:39:29 +0100

    Added functionality for select

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,18 @@
 //Los Angeles
 function LosAngelesDate() {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  losAngelesDateElement.innerHTML = losAngelesTime.format("dddd, MMMM Do YYYY");
-  function displayLosAngelesTime() {
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
     let losAngelesTime = moment().tz("America/Los_Angeles");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format(
-      "hh:mm:ss [<small>]a[</small>]"
-    );
+    losAngelesDateElement.innerHTML =
+      losAngelesTime.format("dddd, MMMM Do YYYY");
+    function displayLosAngelesTime() {
+      let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+      let losAngelesTime = moment().tz("America/Los_Angeles");
+      losAngelesTimeElement.innerHTML = losAngelesTime.format(
+        "HH:mm:ss [<small>]a[</small>]"
+      );
+    }
   }
   displayLosAngelesTime();
   setInterval(displayLosAngelesTime, 1000);
@@ -19,15 +22,17 @@ LosAngelesDate();
 //São Paulo
 function SaoPauloDate() {
   let saoPauloElement = document.querySelector("#sao-paulo");
-  let saoPauloDateElement = saoPauloElement.querySelector(".date");
-  let saoPauloTime = moment().tz("America/Sao-Paulo");
-  saoPauloDateElement.innerHTML = saoPauloTime.format("dddd, MMMM Do YYYY");
-  function displaySaoPauloTime() {
-    let saoPauloTimeElement = saoPauloElement.querySelector(".time");
-    let saoPauloTime = moment().tz("America/Sao-Paulo");
-    saoPauloTimeElement.innerHTML = saoPauloTime.format(
-      "hh:mm:ss [<small>]a[</small>]"
-    );
+  if (saoPauloElement) {
+    let saoPauloDateElement = saoPauloElement.querySelector(".date");
+    let saoPauloTime = moment().tz("America/Sao_Paulo");
+    saoPauloDateElement.innerHTML = saoPauloTime.format("dddd, MMMM Do YYYY");
+    function displaySaoPauloTime() {
+      let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+      let saoPauloTime = moment().tz("America/Sao_Paulo");
+      saoPauloTimeElement.innerHTML = saoPauloTime.format(
+        "HH:mm:ss [<small>]a[</small>]"
+      );
+    }
   }
   displaySaoPauloTime();
   setInterval(displaySaoPauloTime, 1000);
@@ -37,17 +42,42 @@ SaoPauloDate();
 //Lisbon
 function LisbonDate() {
   let lisbonElement = document.querySelector("#lisbon");
-  let lisbonDateElement = lisbonElement.querySelector(".date");
-  let lisbonTime = moment().tz("Europe/Lisbon");
-  lisbonDateElement.innerHTML = lisbonTime.format("dddd, MMMM Do YYYY");
-  function displayLisbonTime() {
-    let lisbonTimeElement = lisbonElement.querySelector(".time");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
     let lisbonTime = moment().tz("Europe/Lisbon");
-    lisbonTimeElement.innerHTML = lisbonTime.format(
-      "hh:mm:ss [<small>]a[</small>]"
-    );
+    lisbonDateElement.innerHTML = lisbonTime.format("dddd, MMMM Do YYYY");
+    function displayLisbonTime() {
+      let lisbonTimeElement = lisbonElement.querySelector(".time");
+      let lisbonTime = moment().tz("Europe/Lisbon");
+      lisbonTimeElement.innerHTML = lisbonTime.format(
+        "HH:mm:ss [<small>]a[</small>]"
+      );
+    }
   }
   displayLisbonTime();
   setInterval(displayLisbonTime, 1000);
 }
 LisbonDate();
+
+function updateCity(event) {
+  function updateTime() {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = ` <div class="city">
+            <div>
+              <h2>${cityName}</h2>
+              <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+            </div>
+            <div class="time">${cityTime.format(
+              "HH:mm:ss [<small>]a[</small>]"
+            )}</div>
+          </div>`;
+  }
+  updateTime();
+  setInterval(updateTime, 1000);
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
